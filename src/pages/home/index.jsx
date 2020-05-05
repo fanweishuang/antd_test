dc31b9364b9728c1d3079ea5e8ca2d197ccb4ce0
weishuang fan <1085987901@qq.com>
2020-05-06 01:03:07 +0800

    尝试假数据失败

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class Home extends Component {
     componentDidMount() {
         document.querySelector('title').innerHTML = "欢迎home"
         console.log(document.querySelector('title'))
+        axios({
+            method: 'get',
+            url: 'http://localhost:8080/data',
+        }).then(res => { console.log(res) })
+
     }
     render() {
         return (
